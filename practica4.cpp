c464f3bbf11c8688b0f21ae63ae48606ac9d309f
xnth13 <117959563+xnth13@users.noreply.github.com>
2023-01-30 13:04:43 -0600

    Add files via upload

---
+++ practica4.cpp
@@ -0,0 +1,57 @@
+// practica4.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int iv;
+	double prod_esca;
+	double producto_Cruz_BxC;
+	double vectorI, vectorJ, vectorK, im, jm, km;
+	float vectorA[3];
+	float vectorB[3];
+	float vectorC[3];
+
+	//llenar valores de vector A
+	for (int iv = 0; iv < 3; iv++){
+		cout << "ingrese los valores del vector1:"<< endl;
+		cin >> vectorA[iv];
+	}
+	//llenar valores de vector B
+
+	for (int iv = 0; iv < 3; iv++){
+		cout << "ingrese los valores del vector 2:"<< endl;
+		cin >> vectorB[iv];
+	}
+	//llenar valores de vector C
+
+	for (int iv = 0; iv < 3; iv++){
+		cout << "ingrese los valores del vector 3:"<< endl;
+		cin >> vectorC[iv];
+	}
+	// PRODUCTO CRUZ B x C ((y2 * z3) - (z2 * y3) - ((x2 * z3) - (y2*x3)) + ((x2*y3) - (y2*x3))
+	vectorI = ((vectorB[1] * vectorC[2]) - (vectorB[2]* vectorC[1]));
+	vectorJ = ((vectorB[0] * vectorC[2]) - (vectorB[2] * vectorC[0]));
+	vectorK = ((vectorB[0] * vectorC[1]) - (vectorB[1] * vectorC[0]));
+	// producto_Cruz_BxC = vectorI - vectorJ + vectorK
+	im = vectorA[0] * vectorI;
+	jm = vectorA[1] * vectorJ;
+	km = vectorA[2] * vectorK;
+	//sumar el resultado de las multiplicaciones
+	prod_esca = im - jm + km;
+	cout << "\n\n";
+	// mostrar el resultado
+	cout << "El vector I" << vectorI << "\n";
+	cout << "El vector J " << vectorJ << "\n";
+	cout << "El vector K: " << vectorK << "\n";
+
+	//resultado "producto escalar"
+	cout << "El producto escalar triple es: " << prod_esca;
+
+
+
+
+	return 0;
+}
+
